26498e77abf8faa8ed4128d50a57609a52b50154
riking <rikingcoding@gmail.com>
2013-08-16 15:31:25 -0700

    Use the typed Collections.emptyList() instead of typeless constant

--- src/main/java/tk/thundaklap/enchantism/EnchantPage.java
+++ src/main/java/tk/thundaklap/enchantism/EnchantPage.java
@@ -1,7 +1,6 @@
 package tk.thundaklap.enchantism;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
--- src/main/java/tk/thundaklap/enchantism/Utils.java
+++ src/main/java/tk/thundaklap/enchantism/Utils.java
@@ -2,18 +2,18 @@ package tk.thundaklap.enchantism;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.bukkit.Material;
+import org.bukkit.Location;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 
-import static java.util.Collections.EMPTY_LIST;
-import org.bukkit.Location;
-
 public final class Utils {
+    private static final List<Enchantment> empty = Collections.<Enchantment>emptyList();
     private static Map<Material, List<Enchantment>> cachedEnchantmentLists = new HashMap<Material, List<Enchantment>>();
 
     static {
@@ -31,11 +31,11 @@ public final class Utils {
      */
     public static List<Enchantment> getEnchantments(ItemStack item) {
         if (item == null) {
-            return EMPTY_LIST;
+            return empty;
         }
         Material type = item.getType();
         if (type == Material.AIR) {
-            return EMPTY_LIST;
+            return empty;
         }
 
         if (cachedEnchantmentLists.containsKey(type)) {
@@ -54,41 +54,41 @@ public final class Utils {
         }
 
         if (ret.size() == 0) {
-            cachedEnchantmentLists.put(type, EMPTY_LIST);
-            return EMPTY_LIST;
+            cachedEnchantmentLists.put(type, empty);
+            return empty;
         }
 
         cachedEnchantmentLists.put(type, ret);
         return ret;
     }
-    
+
     /**
      * Get the amount of bookshelves that an enchantment table would identify as bookshelves.
-     * 
+     *
      * @param loc The location from where the calculation should be made.
      * @return The number of bookshelves that would apply.
      */
     public static int getApplicableBookshelves(Location loc){
-        
+
         int numBookshelves = 0;
-        
+
         // A vanilla enchantment table will accept bookshelves from anywhere in a
         // 2-block square radius from it, and up to one block above.
-        
+
         // TODO: Make bookshelves obstructed by air not count.
-        
+
         for(int x = -2; x <= 2; x++){
             for(int z = -2; z <= 2; z++){
                 if(loc.getWorld().getBlockAt(loc.getBlockX() + x, loc.getBlockY(), loc.getBlockZ() + z).getType().equals(Material.BOOKSHELF)){
                     numBookshelves++;
                 }
-                
+
                 if(loc.getWorld().getBlockAt(loc.getBlockX() + x, loc.getBlockY() + 1, loc.getBlockZ() + z).getType().equals(Material.BOOKSHELF)){
                     numBookshelves++;
                 }
             }
         }
-        
+
         return numBookshelves;
     }
 }
