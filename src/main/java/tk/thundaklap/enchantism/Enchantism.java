69fcb5c6e304a0894278dd4615090684f5011135
Thundaklap <timov@outlook.com>
2013-09-13 17:08:03 +1200

    Remove debug code.

--- src/main/java/tk/thundaklap/enchantism/Enchantism.java
+++ src/main/java/tk/thundaklap/enchantism/Enchantism.java
@@ -24,8 +24,6 @@ public class Enchantism extends JavaPlugin {
 
     public void onDisable() {
         
-        System.out.println(openInventories);
-        
         for(Player p : openInventories.keySet()){
             p.closeInventory();
         }
